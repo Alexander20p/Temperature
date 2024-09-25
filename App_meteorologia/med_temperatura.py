8a99854f31270a247accd3d725fd3cf23be4bb35
Alexander20p <tabletjando@gmail.com>
2024-09-25 14:54:02 +0200

    Update med_temperatura.py

--- App_meteorologia/med_temperatura.py
+++ App_meteorologia/med_temperatura.py
@@ -1,8 +1,8 @@
 import requests as rq
 def pregunt_temp():
-    api = "162e3d3b48451b45cb62c1a4d80277ad"
+    api = "Tu api key"
     url = f"https://api.weatherstack.com/current?access_key={api}"
-    ubicacion = {"query":"toledo Spain"}
+    ubicacion = {"query":"tu ciudad"}
     response = rq.get(url,params=ubicacion)
     resultado = response.json()
     resultado = resultado["current"]
